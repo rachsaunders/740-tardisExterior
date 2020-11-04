abb838cd129d063ce1817ba94c5454373217c60b
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-11-04 06:32:00 +0000

    Toast is working

--- 740-tardisExterior/Tardis.swift
+++ 740-tardisExterior/Tardis.swift
@@ -1,5 +1,5 @@
 //
-//  Tardis.swift
+//  TardisBox.swift
 //  740-tardisExterior
 //
 //  Created by Rachel Saunders on 01/11/2020.
@@ -10,7 +10,7 @@ import Foundation
 import SceneKit
 import ARKit
 
-class Tardis: SCNScene {
+class TardisBox: SCNScene {
     
     // Special nodes used to control animations of the model
     private let contentRootNode = SCNNode()
@@ -27,7 +27,7 @@ class Tardis: SCNScene {
     private let focusNodeBasePosition = simd_float3(0, 0.1, 0.25)
     private var lastRelativePosition: RelativeCameraPositionToHead = .tooHighOrLow
     
-  
+    
     
     // Enums to describe the current state
     private enum RelativeCameraPositionToHead {
@@ -46,10 +46,10 @@ class Tardis: SCNScene {
         case outsideTargetLockDistance
     }
     
-    private enum TardisAnimationState {
+    private enum TardisBoxAnimationState {
         case tardisboxClosed
         case tardisboxOpened
-
+        
     }
     
     // MARK: - Initialization and Loading
@@ -58,8 +58,8 @@ class Tardis: SCNScene {
         super.init()
         
         // Load the environment map
-   //     self.lightingEnvironment.contents = UIImage(named: "")!
-
+        //     self.lightingEnvironment.contents = UIImage(named: "")!
+        
         loadModel()
     }
     
@@ -78,8 +78,178 @@ class Tardis: SCNScene {
         }
         self.rootNode.addChildNode(contentRootNode)
         contentRootNode.addChildNode(wrapperNode)
-       
+        
         modelLoaded = true
     }
     
+    // MARK: - Public API
+    
+    func show() {
+        contentRootNode.isHidden = false
+    }
+    
+    func hide() {
+        contentRootNode.isHidden = true
+    }
+    
+    func isVisible() -> Bool {
+        return !contentRootNode.isHidden
+    }
+    
+    func setTransform(_ transform: simd_float4x4) {
+        contentRootNode.simdTransform = transform
+    }
 }
+
+
+
+// MARK: - React To Placement and Tap
+
+extension TardisBox {
+    
+    func reactToPositionChange(in view: ARSCNView) {
+        self.reactToPlacement(in: view)
+    }
+    
+    func reactToInitialPlacement(in view: ARSCNView) {
+        self.reactToPlacement(in: view, isInitial: true)
+    }
+    
+    private func reactToPlacement(in sceneView: ARSCNView, isInitial: Bool = false) {
+        if isInitial {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+                self.getColorFromEnvironment(sceneView: sceneView)
+                self.activateCamouflage(true)
+            })
+        } else {
+            DispatchQueue.main.async {
+                self.updateCamouflage(sceneView: sceneView)
+            }
+        }
+    }
+    
+    func reactToTap(in sceneView: ARSCNView) {
+        self.activateCamouflage(false)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            self.activateCamouflage(true)
+        })
+    }
+    
+    private func activateCamouflage(_ activate: Bool) {
+        
+        let blendFactor = activate ? 1.0 : 0.0
+        
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 1.5
+     
+        SCNTransaction.commit()
+    }
+    
+    private func updateCamouflage(sceneView: ARSCNView) {
+        getColorFromEnvironment(sceneView: sceneView)
+        
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 1.5
+        SCNTransaction.commit()
+    }
+    
+    private func getColorFromEnvironment(sceneView: ARSCNView) {
+        let worldPos = sceneView.projectPoint(contentRootNode.worldPosition)
+        
+        
+    }
+}
+
+// MARK: - React To Rendering
+
+extension TardisBox {
+    
+    func reactToRendering(in sceneView: ARSCNView) {
+        // Update environment map to match ambient light level
+        lightingEnvironment.intensity = (sceneView.session.currentFrame?.lightEstimate?.ambientIntensity ?? 1000) / 100
+        
+    
+    
+    
+}
+}
+    
+// MARK: - React To DidApplyConstraints
+
+extension TardisBox {
+    
+    func reactToDidApplyConstraints(in sceneView: ARSCNView) {
+        guard modelLoaded, let pointOfView = sceneView.pointOfView else {
+            return
+        }
+        
+        // Correct the user position such that it is a few centimeters in front of the camera.
+        let translationLocal = SCNVector3(0, 0, -0.012)
+        let translationWorld = pointOfView.convertVector(translationLocal, to: nil)
+        let camTransform = SCNMatrix4Translate(pointOfView.transform, translationWorld.x, translationWorld.y, translationWorld.z)
+        let userPosition = simd_float3(camTransform.m41, camTransform.m42, camTransform.m43)
+        
+        
+    }
+    
+    
+}
+
+// MARK: - Helper functions
+
+extension TardisBox {
+    
+    private func rad(_ deg: Float) -> Float {
+        return deg * Float.pi / 180
+    }
+    
+    private func randomlyUpdate(_ vector: inout simd_float3) {
+        switch arc4random() % 400 {
+        case 0: vector.x = 0.1
+        case 1: vector.x = -0.1
+        case 2: vector.y = 0.1
+        case 3: vector.y = -0.1
+        case 4, 5, 6, 7: vector = simd_float3()
+        default: break
+        }
+    }
+    
+    private func setupSpecialNodes() {
+        // Retrieve nodes we need to reference for animations.
+        geometryRoot = self.rootNode.childNode(withName: "TardisBox", recursively: true)
+        
+        skin = geometryRoot.geometry?.materials.first
+        
+        // Fix materials
+        geometryRoot.geometry?.firstMaterial?.lightingModel = .physicallyBased
+        geometryRoot.geometry?.firstMaterial?.roughness.contents = "art.scnassets/textures/tardisBox_ROUGHNESS.png"
+        let shadowPlane = self.rootNode.childNode(withName: "Shadow", recursively: true)
+        shadowPlane?.castsShadow = false
+        
+    }
+    
+    private func setupConstraints() {
+        // not needed
+    }
+    
+    private func resetState() {
+        // not needed
+    }
+    
+    private func setupShader() {
+        guard let path = Bundle.main.path(forResource: "skin", ofType: "shaderModifier", inDirectory: "art.scnassets"),
+              let shader = try? String(contentsOfFile: path, encoding: String.Encoding.utf8) else {
+            return
+        }
+        
+        skin.shaderModifiers = [SCNShaderModifierEntryPoint.surface: shader]
+        
+        skin.setValue(Double(0), forKey: "blendFactor")
+        skin.setValue(NSValue(scnVector3: SCNVector3Zero), forKey: "skinColorFromEnvironment")
+        
+        let sparseTexture = SCNMaterialProperty(contents: UIImage(named: "art.scnassets/textures/tardisBox_DIFFUSE_BASE.png")!)
+        skin.setValue(sparseTexture, forKey: "sparseTexture")
+    }
+}
+
+
--- 740-tardisExterior/ViewController.swift
+++ 740-tardisExterior/ViewController.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     //MARK:- OUTLETS
     @IBOutlet var sceneView: ARSCNView!
     
@@ -21,7 +21,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     //MARK:- VARS
     
-    var tardisbox = Tardis()
+    var tardisbox = TardisBox()
     
     //MARK:- VIEWDIDLOAD
     
@@ -30,6 +30,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         sceneView.scene = tardisbox
+        
+        sceneView.automaticallyUpdatesLighting = false
         
         
     }
@@ -44,7 +46,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         
         UIApplication.shared.isIdleTimerDisabled = true
-
+        
         startNewSession()
     }
     
@@ -55,11 +57,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    //MARK:- START NEW SESSION
     func startNewSession(){
         
         self.toast.alpha = 0
         self.toast.frame = self.toast.frame.insetBy(dx: 5, dy: 5)
         
+        tardisbox.hide()
         
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = .horizontal
@@ -67,35 +71,116 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
     }
     
-    //    MARK:- CAMERA TRACKING STATES
+    //MARK:- IB ACTIONS
+    
+    @IBAction func didPan(_ recognizer: UIPanGestureRecognizer) {
+        let location = recognizer.location(in: sceneView)
+        
+        // Drag the object on an infinite plane
+        let arHitTestResult = sceneView.hitTest(location, types: .existingPlane)
+        if !arHitTestResult.isEmpty {
+            let hit = arHitTestResult.first!
+            tardisbox.setTransform(hit.worldTransform)
+            
+            if recognizer.state == .ended {
+                print("ended")
+            }
+        }
+    }
+    
+    
+    @IBAction func didTap(_ recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: sceneView)
+        
+        // When tapped on the object, call the object's method to react on it
+        let sceneHitTestResult = sceneView.hitTest(location, options: nil)
+        if !sceneHitTestResult.isEmpty {
+            // We only have one content, so we know which node was hit.
+            // If the scene contains multiple objects, you would need to check here if the right node was hit
+            tardisbox.reactToTap(in: sceneView)
+            return
+        }
+        
+        // When tapped on a plane, reposition the content
+        let arHitTestResult = sceneView.hitTest(location, types: .existingPlane)
+        if !arHitTestResult.isEmpty {
+            let hit = arHitTestResult.first!
+            tardisbox.setTransform(hit.worldTransform)
+            
+        }
+    }
+    
+    
+}
+
+
+// : ARSCNViewDelegate removed
+
+extension ViewController {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if tardisbox.isVisible() { return }
+        
+        // Unhide the content and position it on the detected plane
+        if anchor is ARPlaneAnchor {
+            tardisbox.setTransform(anchor.transform)
+            tardisbox.show()
+            tardisbox.reactToInitialPlacement(in: sceneView)
+            
+            DispatchQueue.main.async {
+                self.hideToast()
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        tardisbox.reactToRendering(in: sceneView)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didApplyConstraintsAtTime time: TimeInterval) {
+        tardisbox.reactToDidApplyConstraints(in: sceneView)
+    }
+}
+
+extension ViewController: ARSessionObserver {
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        showToast("Session was interrupted")
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        startNewSession()
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        showToast("Session failed: \(error.localizedDescription)")
+        startNewSession()
+    }
     
     func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
-        
-        // SWITCH CASE TO DO ABOUT MESSAGES TOAST WILL SAY
         var message: String? = nil
         
         switch camera.trackingState {
         case .notAvailable:
-            message = "tracking not available"
+            message = "Tracking not available"
         case .limited(.initializing):
-            message = "Initialising AR..."
+            message = "Initializing AR session"
         case .limited(.excessiveMotion):
-            message = "Stop moving!"
+            message = "Too much motion"
         case .limited(.insufficientFeatures):
             message = "Not enough surface details"
         case .normal:
-            // To do
-            print("you need to complete this Rach!")
+            if !tardisbox.isVisible() {
+                message = "Move to find a horizontal surface"
+            }
         default:
+            // We are only concerned with the tracking states above.
             message = "Camera changed tracking state"
-            
         }
         
         message != nil ? showToast(message!) : hideToast()
     }
 }
-
-//MARK:- TOAST SETTINGS
 
 extension ViewController {
     
@@ -105,13 +190,22 @@ extension ViewController {
         guard toast.alpha == 0 else {
             return
         }
+        
+        toast.layer.masksToBounds = true
+        toast.layer.cornerRadius = 7.5
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.toast.alpha = 1
+            self.toast.frame = self.toast.frame.insetBy(dx: -5, dy: -5)
+        })
+        
     }
     
     func hideToast() {
-        UIView.animate(withDuration: 0.25) {
+        UIView.animate(withDuration: 0.25, animations: {
             self.toast.alpha = 0
-        }
-
+            self.toast.frame = self.toast.frame.insetBy(dx: 5, dy: 5)
+        })
     }
-    
 }
+
